607bef828ed9d776ceb6927d27d04dc0d3809e50
AngelBotsD <angelperez22448@gmail.com>
2025-11-14 18:48:50 -0600

    Actualizar stickers-brat.js

--- plugins/stickers-brat.js
+++ plugins/stickers-brat.js
@@ -5,28 +5,43 @@ const handler = async (m, { conn, text }) => {
   }
 
   if (!text) {
-    return m.reply(`☁️ *𝙰𝚐𝚛𝚎𝚐𝚊 𝚝𝚎𝚡𝚝𝚘 𝚘 𝚛𝚎𝚜𝚙𝚘𝚗𝚍𝚎 𝚊 𝚞𝚗 𝚖𝚎𝚗𝚜𝚊𝚓𝚎 𝚙𝚊𝚛𝚊 𝚌𝚛𝚎𝚊𝚛 𝚎𝚕 𝚜𝚝𝚒𝚌𝚔𝚎𝚛*.`)
+    return m.reply(
+      `𝖠𝗀𝗋𝖾𝗀𝖺 𝖳𝖾𝗑𝗍𝗈 𝖮 𝖱𝖾𝗌𝗉𝗈𝗇𝖽𝖾 𝖠 𝖴𝗇 𝖬𝖾𝗇𝗌𝖺𝗃𝖾 𝖯𝖺𝗋𝖺 𝖢𝗋𝖾𝖺𝗋 𝖤𝗅 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖡𝗋𝖺𝗍`,
+      global.rcanal
+    )
   }
 
   try {
     await conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } })
 
     const url = `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(text)}`
+
+    // === ENTREGA DEL STICKER + global.rcanal ===
     await conn.sendMessage(
       m.chat,
       {
         sticker: { url },
         packname: "",
         author: "",
+        ...global.rcanal
       },
       { quoted: m }
     )
 
     await conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
+
   } catch (e) {
     console.error(e)
+
     await conn.sendMessage(m.chat, { react: { text: "❌", key: m.key } })
-    conn.reply(m.chat, '❌ *𝙴𝚛𝚛𝚘𝚛 𝙰𝚕 𝙶𝚎𝚗𝚎𝚛𝚊𝚛 𝚎𝚕 𝚂𝚝𝚒𝚌𝚔𝚎𝚛*.', m)
+
+    // === MENSAJE DE ERROR + global.rcanal ===
+    conn.reply(
+      m.chat,
+      '❌ *𝙴𝚛𝚛𝚘𝚛 𝙰𝚕 𝙶𝚎𝚗𝚎𝚛𝚊𝚛 𝚎𝚕 𝚂𝚝𝚒𝚌𝚔𝚎𝗋*.',
+      m,
+      global.rcanal
+    )
   }
 }
 
