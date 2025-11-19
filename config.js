2ebc251e7201ed0a89e52d2c7a3c64e5f315e0fe
AngelBotsD <angelperez22448@gmail.com>
2025-11-18 21:00:55 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = ['38354561278087', '25856038715509', '']
+global.owner = ['38354561278087', '25856038715509', '181613363249345', '']
 
 global.mods = []
 global.prems = []
