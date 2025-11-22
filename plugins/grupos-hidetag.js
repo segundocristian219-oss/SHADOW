b65b620b0764877678f2ee7cdb4a80fbcde5c230
AngelBotsD <angelperez22448@gmail.com>
2025-11-22 12:35:28 -0600

    Actualizar grupos-hidetag.js

--- plugins/grupos-hidetag.js
+++ plugins/grupos-hidetag.js
@@ -105,6 +105,8 @@ const handler = async (m, { conn, participants }) => {
   }
 }
 
+handler.help = ["𝖭𝗈𝗍𝗂𝖿𝗒"];
+handler.tags = ["𝖦𝖱𝖴𝖯𝖮𝖲"];
 handler.customPrefix = /^\.?n(\s|$)/i
 handler.command = new RegExp()
 handler.group = true
